53fc35547483c5af89b25efaacc27c9536a016a4
y_uuki <yuki.tsubo@gmail.com>
2014-11-13 09:04:32 +0900

    port 指定わすれ

--- server.sh
+++ server.sh
@@ -1,4 +1,5 @@
 exec carton exec plackup -s Starlet                                \
+    --port=8080
     --max-reqs-per-child=50000 --min-reqs-per-child=5000 \
     --max-workers=8 \
     -E prod app.psgi
