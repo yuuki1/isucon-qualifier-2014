404da71934c996ccd3b88d6ad5792e941355a694
y_uuki <yuki.tsubo@gmail.com>
2014-11-13 09:24:23 +0900

    tuning Starlet

--- server.sh
+++ server.sh
@@ -1,5 +1,6 @@
 exec carton exec plackup -s Starlet                                \
     --port=8080
-    --max-reqs-per-child=50000 --min-reqs-per-child=5000 \
-    --max-workers=8 \
+    --backlog=16384
+    --max-reqs-per-child=500000 --min-reqs-per-child=400000 \
+    --max-workers=4 \
     -E prod app.psgi
